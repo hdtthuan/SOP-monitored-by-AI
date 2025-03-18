6286164af8c7f567a63306607579b1d42ba37922
TuanTony5 <phutuan05122004@gmail.com>
2025-03-18 11:14:37 +0700

    return action status of process_frame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import cv2
 import logging
+
 logging.getLogger("ultralytics").setLevel(logging.ERROR)
 from ultralytics import YOLO
 import mediapipe as mp
@@ -9,17 +10,22 @@ from modules.hand_detection_v3 import process_frame
 from modules.roi_selection import roi_selection_loop
 
 
-
-
-
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
 parser = argparse.ArgumentParser(description="ROI selection and hand detection.")
-parser.add_argument("--source", type=str, default="./data/Val_video.mp4",
-                    help="Path to video file or camera index (default: 0)")
-parser.add_argument("--yolo_weight", type=str, default="./models/last_bk0.3.pt",
-                    help="Path to video file or camera index (default: 0)")
+parser.add_argument(
+    "--source",
+    type=str,
+    default="./data/Val_video.mp4",
+    help="Path to video file or camera index (default: 0)",
+)
+parser.add_argument(
+    "--yolo_weight",
+    type=str,
+    default="./models/last_bk0.3.pt",
+    help="Path to video file or camera index (default: 0)",
+)
 args = parser.parse_args()
 
 try:
@@ -35,15 +41,15 @@ if not cap.isOpened():
 roi_selection_loop(source)
 
 hands_detector = mp_hands.Hands(
-        static_image_mode=False,
-        max_num_hands=2,
-        min_detection_confidence=0.7
-    )
+    static_image_mode=False, max_num_hands=2, min_detection_confidence=0.7
+)
 
 model = YOLO(args.yolo_weight)
 
 while True:
-    ret, frame = cap.read() # TODO: do you see variable frame ?, it can be input, and you can use cv2 draw into this frame inside function.
+    ret, frame = (
+        cap.read()
+    )  # TODO: do you see variable frame ?, it can be input, and you can use cv2 draw into this frame inside function.
     if not ret:
         break
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -61,11 +67,12 @@ while True:
     #     for hand_landmarks in results_hands.multi_hand_landmarks:
     #         print(hand_landmarks)
 
-    process_frame(frame)
+    action_status = process_frame(frame)
+    print(action_status)
 
     cv2.imshow("Hand Detection", frame)
     key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
+    if key == ord("q"):
         break
 
 cap.release()
